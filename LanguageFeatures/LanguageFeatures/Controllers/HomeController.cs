b15232dc94ae7e44c3cb129ff4cbf7c8deea93b6
k.dobrogost <dante.3.kd@gmail.cojm>
2019-06-02 14:20:00 +0200

    Opracowywanie LanguageFeatures

--- LanguageFeatures/LanguageFeatures/Controllers/HomeController.cs
+++ LanguageFeatures/LanguageFeatures/Controllers/HomeController.cs
@@ -3,6 +3,7 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Threading.Tasks;
 using Microsoft.AspNetCore.Mvc;
+using LanguageFeatures.Models;
 
 namespace LanguageFeatures.Controllers
 {
@@ -10,7 +11,21 @@ namespace LanguageFeatures.Controllers
     {
         public ViewResult Index()
         {
-            return View(new string[] { "C#", "Język", "Cechy" });
+            string[] names = new string[3];
+            names
+
+
+            //List<string> results = new List<string>();
+
+            //foreach (Product p in Product.GetProducts())
+            //{
+            //    string name = p?.Name ?? "<brak>";
+            //    decimal? price = p?.Price ?? 0;
+            //    string realtedName = p?.Related?.Name ?? "<brak>";
+            //    results.Add(string.Format($"Produkt: {name}, cena: {price}, powiązanie {realtedName}"));
+            //}
+
+            //return View(results);
         }
     }
 }
--- LanguageFeatures/LanguageFeatures/Models/Product.cs
+++ LanguageFeatures/LanguageFeatures/Models/Product.cs
@@ -7,22 +7,33 @@ namespace LanguageFeatures.Models
 {
     public class Product
     {
+        public Product(bool stock = true)
+        {
+            InStock = stock;
+        }
+
         public string Name { get; set; }
+        public string Category { get; set; } = "Wodniarstwo";
         public decimal? Price { get; set; }
+        public Product Related { get; set; }
+        public bool InStock { get; } = true;
 
         public static Product[] GetProducts()
         {
             Product kayak = new Product
             {
                 Name = "Kajak",
+                Category = "Sporty Wodne",
                 Price = 275M
             };
 
-            Product lifejacket = new Product
+            Product lifejacket = new Product(false)
             {
                 Name = "Kamizelka ratunkowa",
                 Price = 48.95M
             };
+
+            kayak.Related = lifejacket;
 
             return new Product[] { kayak, lifejacket, null };
         }
